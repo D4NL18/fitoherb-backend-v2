920a0056fe64dfb5792ce783ed9377a1e88534bf
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2026-04-02 11:44:08 -0300

    feat: auditor fields and listening

--- src/main/java/com/fitoherb/fitoherb_backend_v2/FitoherbBackendV2Application.java
+++ src/main/java/com/fitoherb/fitoherb_backend_v2/FitoherbBackendV2Application.java
@@ -2,8 +2,10 @@ package com.fitoherb.fitoherb_backend_v2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
 public class FitoherbBackendV2Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/fitoherb/fitoherb_backend_v2/entities/User.java
+++ src/main/java/com/fitoherb/fitoherb_backend_v2/entities/User.java
@@ -3,11 +3,17 @@ package com.fitoherb.fitoherb_backend_v2.entities;
 import com.fitoherb.fitoherb_backend_v2.enums.UserRole;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.List;
 
@@ -18,6 +24,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
+@EntityListeners(AuditingEntityListener.class)
 public class User implements UserDetails {
 
     @Id
@@ -38,6 +45,22 @@ public class User implements UserDetails {
 
     @Column(nullable = false)
     private UserRole role;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @CreatedBy
+    @Column(name = "created_by", updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "updated_by")
+    private String updatedBy;
 
     public User(String email, String encryptedPassword, String name, LocalDate birthDate, UserRole role) {
         this.email = email;
