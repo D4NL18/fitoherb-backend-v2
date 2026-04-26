8854d2b3316472be7d107b36f212be46345107af
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2026-04-26 20:27:00 -0300

    feat: refresh token endpoint

--- src/main/java/com/fitoherb/fitoherb_backend_v2/controllers/AuthController.java
+++ src/main/java/com/fitoherb/fitoherb_backend_v2/controllers/AuthController.java
@@ -19,6 +19,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -28,31 +29,16 @@ import java.net.URI;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("auth")
-@Tag(name = "Auth", description = "Endpoints for Identity and Access Management (IAM). " +
-        "Handles user registration and secure authentication via JWT (JSON Web Tokens).")
+@Tag(name = "Auth", description = "Endpoints for Identity and Access Management (IAM). Handles user registration and secure authentication via JWT (JSON Web Tokens).")
 public class AuthController {
 
     private final AuthorizationService authService;
 
-    @Operation(
-            summary = "Authenticate user",
-            description = "Validates user credentials (email and password) and returns a Bearer Token (JWT) for authorized access."
-    )
+    @Operation(summary = "Authenticate user", description = "Validates user credentials (email and password) and returns a Bearer Token (JWT) for authorized access.")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Successfully authenticated",
-                    content = @Content(schema = @Schema(implementation = LoginRes.class))),
-            @ApiResponse(responseCode = "401", description = "Invalid credentials",
-                    content = @Content(schema = @Schema(implementation = RestErrorMessage.class),
-                            examples = @ExampleObject(
-                                    name = "Invalid Credentials",
-                                    value = "{\"status\": \"UNAUTHORIZED\", \"message\": \"E-mail or password invalid.\"}"
-                            ))),
-            @ApiResponse(responseCode = "400", description = "Validation failed",
-                    content = @Content(schema = @Schema(implementation = RestValidationErrorMessage.class),
-                            examples = @ExampleObject(
-                                    name = "Field Validation Error",
-                                    value = "{\"status\": \"BAD_REQUEST\", \"message\": \"Validation failed for one or more fields\", \"errors\": {\"email\": \"must be a well-formed email address\", \"password\": \"must not be blank\"}}"
-                            )))
+            @ApiResponse(responseCode = "200", description = "Successfully authenticated", content = @Content(schema = @Schema(implementation = LoginRes.class))),
+            @ApiResponse(responseCode = "401", description = "Invalid credentials", content = @Content(schema = @Schema(implementation = RestErrorMessage.class), examples = @ExampleObject(name = "Invalid Credentials", value = "{\"status\": \"UNAUTHORIZED\", \"message\": \"E-mail or password invalid.\"}"))),
+            @ApiResponse(responseCode = "400", description = "Validation failed", content = @Content(schema = @Schema(implementation = RestValidationErrorMessage.class), examples = @ExampleObject(name = "Field Validation Error", value = "{\"status\": \"BAD_REQUEST\", \"message\": \"Validation failed for one or more fields\", \"errors\": {\"email\": \"must be a well-formed email address\", \"password\": \"must not be blank\"}}")))
     })
     @PostMapping("/login")
     public ResponseEntity<LoginRes> login(@RequestBody @Valid LoginReq loginReq) {
@@ -60,30 +46,12 @@ public class AuthController {
         return ResponseEntity.ok(new LoginRes(token));
     }
 
-    @Operation(
-            summary = "Register new user",
-            description = "Creates a new user account in the system database and returns the location URI of the newly created resource."
-    )
+    @Operation(summary = "Register new user", description = "Creates a new user account in the system database and returns the location URI of the newly created resource.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "User registered successfully"),
-            @ApiResponse(responseCode = "409", description = "Conflict - Duplicate E-mail",
-                    content = @Content(schema = @Schema(implementation = RestErrorMessage.class),
-                            examples = @ExampleObject(
-                                    name = "Email Conflict",
-                                    value = "{\"status\": \"CONFLICT\", \"message\": \"E-mail already in use\"}"
-                            ))),
-            @ApiResponse(responseCode = "400", description = "Validation error",
-                    content = @Content(schema = @Schema(implementation = RestValidationErrorMessage.class),
-                            examples = @ExampleObject(
-                                    name = "Registration Validation Error",
-                                    value = "{\"status\": \"BAD_REQUEST\", \"message\": \"Validation failed for one or more fields\", \"errors\": {\"birthDate\": \"must be a past date\", \"role\": \"must not be null\"}}"
-                            ))),
-            @ApiResponse(responseCode = "500", description = "Internal error",
-                    content = @Content(schema = @Schema(implementation = RestErrorMessage.class),
-                            examples = @ExampleObject(
-                                    name = "Database Error",
-                                    value = "{\"status\": \"INTERNAL_SERVER_ERROR\", \"message\": \"An unexpected error occurred. Please contact the administrator.\"}"
-                            )))
+            @ApiResponse(responseCode = "409", description = "Conflict - Duplicate E-mail", content = @Content(schema = @Schema(implementation = RestErrorMessage.class), examples = @ExampleObject(name = "Email Conflict", value = "{\"status\": \"CONFLICT\", \"message\": \"E-mail already in use\"}"))),
+            @ApiResponse(responseCode = "400", description = "Validation error", content = @Content(schema = @Schema(implementation = RestValidationErrorMessage.class), examples = @ExampleObject(name = "Registration Validation Error", value = "{\"status\": \"BAD_REQUEST\", \"message\": \"Validation failed for one or more fields\", \"errors\": {\"birthDate\": \"must be a past date\", \"role\": \"must not be null\"}}"))),
+            @ApiResponse(responseCode = "500", description = "Internal error", content = @Content(schema = @Schema(implementation = RestErrorMessage.class), examples = @ExampleObject(name = "Database Error", value = "{\"status\": \"INTERNAL_SERVER_ERROR\", \"message\": \"An unexpected error occurred. Please contact the administrator.\"}")))
     })
     @PostMapping("/register")
     public ResponseEntity<Void> register(@RequestBody @Valid RegisterReq registerReq) {
@@ -96,5 +64,18 @@ public class AuthController {
                 .toUri();
 
         return ResponseEntity.created(location).build();
+    }
+
+    @Operation(summary = "Refresh token", description = "Receives an expired (but validly signed) token and returns a fresh one.")
+    @PostMapping("/refresh")
+    public ResponseEntity<LoginRes> refresh(@RequestHeader(value = "Authorization", required = false) String authHeader) {
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        String token = authHeader.replace("Bearer ", "");
+        String newToken = authService.refreshToken(token);
+
+        return ResponseEntity.ok(new LoginRes(newToken));
     }
 }
--- src/main/java/com/fitoherb/fitoherb_backend_v2/services/TokenService.java
+++ src/main/java/com/fitoherb/fitoherb_backend_v2/services/TokenService.java
@@ -3,6 +3,7 @@ package com.fitoherb.fitoherb_backend_v2.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.fitoherb.fitoherb_backend_v2.entities.User;
 import com.fitoherb.fitoherb_backend_v2.exceptions.InvalidTokenException;
 import org.springframework.beans.factory.annotation.Value;
@@ -26,7 +27,7 @@ public class TokenService {
                     .withSubject(user.getEmail())
                     .withExpiresAt(genExpirationDate())
                     .sign(algorithm);
-        }catch (JWTVerificationException _) {
+        } catch (JWTVerificationException _) {
             throw new InvalidTokenException("Erro ao gerar token JWT");
         }
     }
@@ -39,8 +40,23 @@ public class TokenService {
                     .build()
                     .verify(token)
                     .getSubject();
-        }catch (JWTVerificationException _) {
+        } catch (JWTVerificationException _) {
             throw new InvalidTokenException("Token JWT inválido ou expirado");
+        }
+    }
+
+    public String validateAndGetSubjectEvenIfExpired(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (TokenExpiredException e) {
+            return JWT.decode(token).getSubject();
+        } catch (JWTVerificationException e) {
+            throw new InvalidTokenException("Token JWT inválido ou forjado");
         }
     }
 
