b363b32b7ce58dfafb0035dd37bdc31efc61ad1a
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2026-06-29 18:04:23 -0300

    fix cors for prod url

--- src/main/java/com/fitoherb/fitoherb_backend_v2/infra/config/WebConfig.java
+++ src/main/java/com/fitoherb/fitoherb_backend_v2/infra/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("http://localhost:4200", "https://fitoherb.web.app", "https://fitoherb.firebaseapp.com")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "PATCH")
                 .allowedHeaders("*")
                 .allowCredentials(true);
